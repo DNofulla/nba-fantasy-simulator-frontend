9b0eef4a5d77bed9d82cf5f70991db3a653aaa92
a-kisiel <60425431+a-kisiel@users.noreply.github.com>
2020-09-28 18:02:38 -0400

    MyTournaments Added

--- src/pages/MyTournaments.jsx
+++ src/pages/MyTournaments.jsx
@@ -1,10 +1,39 @@
 import React from "react";
+import TeamInformation from "../components/TeamInformation";
+import TournamentChat from "../components/TournamentChat";
+import MatchHistory from "../components/MatchHistory";
 import NavBar from "../components/NavBar";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/core";
 
 export default function MyTournaments() {
   return (
     <div className="MyTournaments">
       <NavBar />
-    </div>
+        <Tabs variant='enclosed' size='lg'>
+          <TabList>
+            <Tab _selected={{ color: 'white', bg: 'rgb(10, 53, 66)', shadow: 'none' }} 
+                _hover={{ color: 'lightseagreen' }}
+                >Match History</Tab>
+            <Tab _selected={{ color: 'white', bg: 'rgb(10, 53, 66)', shadow: 'none' }} 
+                _hover={{ color: 'lightseagreen' }}
+                >Team Stats</Tab>
+            <Tab _selected={{ color: 'white', bg: 'rgb(10, 53, 66)', shadow: 'none' }} 
+                _hover={{ color: 'lightseagreen' }}
+                >Tournament Chat</Tab>
+          </TabList>
+
+          <TabPanels>
+              <TabPanel eventKey='matchhistory' title='Match History'>
+                <MatchHistory />
+              </TabPanel>
+              <TabPanel eventKey='TeamInformation' title='Team Information'>
+                <TeamInformation />
+              </TabPanel>
+              <TabPanel eventKey='tournamentchat' title='Tournament Chat'>
+                <TournamentChat />
+              </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
   );
 }
